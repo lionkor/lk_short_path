929c2296b9fe934771e1839e6bd90d2ebd05be7c
Lion Kortlepel <development@kortlepel.com>
2020-05-01 01:52:55 +0200

    Remove some whitespace

--- lk_short_path.py
+++ lk_short_path.py
@@ -3,17 +3,14 @@
 import os
 
 path = os.getcwd()
-
 _HOME = os.getenv("HOME")
 
 if path.startswith(_HOME):
     path = path.replace(_HOME, "~")
 
 result = ""
-
 if not path.startswith("~"):
     result += "/" # prefix path with root
-
 for part in path.split('/'):
     if not path.endswith(part):
         result += part[0:1]
